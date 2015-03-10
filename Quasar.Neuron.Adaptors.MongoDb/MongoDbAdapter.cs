710fc3b3f45c2a17a22b443f0ec7e039cf645e6d
sarkarstanmoy <sarkarstanmoy@gmail.com>
2015-03-10 13:58:31 +0530

    Added MongoDb Adapter

---
+++ Quasar.Neuron.Adaptors.MongoDb/MongoDbAdapter.cs
@@ -0,0 +1,330 @@
+﻿using System.ComponentModel;
+using System.Transactions;
+using System;
+using System.Globalization;
+using Neuron.Esb.Pipelines;
+using System.IO.Ports;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+/// <summary>
+/// Namespace for adapter assembly. 
+/// </summary>
+/// <remarks>
+/// The default namespace for the adapter assembly is defined in the project property's application tab. This can be changed.
+/// The name of the assembly, also defined on the application tab of the project property page is the concatonation of the 
+/// namespace + the name of the adapter class.  The adapter class name, "NameAdapter", should be changed to reflect the nature of the adapter. 
+/// For instance, if this adapter was to communicate with SalesForce, "NameAdapter" could be changed to 
+/// "SalesForceAdapter". Hence, in project property's application tab the user would also change the assembly name so that the last
+/// segment of the name would be identical to the class name adopted. The full assembly name would be namespace + "." + class name.
+/// </remarks>
+namespace Neuron.Esb.Adapters
+{
+    /// <summary>
+    /// Generally the developer would change the name of the class to reflect the nature of the adapter. The class name in the 
+    /// Adapter.cs file would also have to be changed to ensure that both class names are identical since they are both marked
+    /// as partial classes.
+    /// </summary>
+    public partial class MongoDbAdapter
+    {
+        #region Constants and types
+        /// <summary>
+        /// These 2 constants are important.  They are used within the Neuron ESB Explorer at runtime and design time for 
+        /// reporting and changing any properties that the custom adapter implementation may need. 
+        /// 
+        /// For example, _metadataOutPrefix constant, is the prefix used for querying or editing any custom properties that the 
+        /// custom adapter may wish to allow users to modify or access at runtime through the context.Data.GetProperty() or 
+        /// context.Data.SetProperty() methods. Also all properties "registered" using this prefix in the adapter class's constructor
+        /// are displayed in the Set Property Process Step.
+        /// 
+        /// For instance, if this class used a connection string to access a resource, that connection string could be provided at 
+        /// design time by defining it as a property of the class.  However, if the developer wanted to grant users the ability to 
+        /// provide that property at runtime (as it may change for each message), the user could register the property in the class's 
+        /// constructor and then with each incoming message, inspect the message for the presence of that property, extract the value 
+        /// and use it at runtime to access the resource.
+        /// 
+        /// The _adapterName constant defines a "user friendly name" that is used to in the Adapter Registration screen within the 
+        /// Neuron ESB Explorer. Its the name listed in the drop down adapter list when registrating an adapter. 
+        /// </summary>
+        private const string MetadataPrefix = "prefix";
+        private const string AdapterName = "MongoDb Adapter";
+        #endregion
+
+        /// <summary>
+        /// There are a number of sample propeties defined. All properties defined will be displayed at design time
+        /// in a property grid within the Adapter Endpoint configuration screen in the Neuron ESB Explorer. 
+        /// These samples demonstrate how to make properties visible/invisible (PropertyAttributesProvider attribute) 
+        /// based on the state of other properties. These samples also demonstrate how to use type converters (TypeConverter attribute)
+        /// , ordering (PropertyOrder attribute) of properties, categories (Category attribute), etc.
+        /// 
+        /// By default, all properties will also be displayed in the Bindings Dialog form for binding to Environmental Variables.
+        /// If a property should not be "bindable", i.e. should not be used with Environmental Variables, add the attribute, 
+        /// ,Bindable(false),  to the property.  The sample Password property has this applied.
+        /// </summary>
+        #region SAMPLE: Public Properties to Expose and Control in UI
+
+              
+
+        private string _database = System.String.Empty;
+        private string _collection = System.String.Empty;
+        private string _connectionstring = "mongodb://localhost";
+        private MongoServer mongo;
+
+        [DisplayName("ConnectionString")]
+        [Category("(General)")]
+        [Description("MongoDb server connection string")]
+        [DefaultValue("mongodb://localhost")]
+        [PropertyOrder(0)]
+        public string ConnectionString
+        {
+            get { return _connectionstring; }
+            set { _connectionstring = value; }
+        }
+
+        [DisplayName("Database Name")]
+        [Category("(General)")]
+        [Description("The name of database for storing messages.")]
+        [DefaultValue("")]
+        [PropertyOrder(1)]
+        public string Database
+        {
+            get { return _database; }
+            set { _database = value; }
+        }
+
+        [DisplayName("Collection Name")]
+        [Category("(General)")]
+        [Description("The name of the collection for storing messages")]
+        [DefaultValue("")]
+        [PropertyOrder(2)]
+        public string Collection
+        {
+            get { return _collection; }
+            set { _collection = value; }
+        }
+
+        private ErrorLevel _level = ErrorLevel.Warning;
+        [DisplayName("Error Level")]
+        [Category("(General)")]
+        [Description("Default error level messages will be written to the log")]
+        [PropertyOrder(3)]
+        public ErrorLevel Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+
+        
+        #endregion
+
+        /// <summary>
+        /// Create an initialized instance of the adapter
+        /// </summary>
+        /// <remarks>
+        /// AdapterModes and ESBAdapterCapabilities must always be initialized so that the ESB
+        /// framework and Neuron ESB Explorer UI can correctly interact with the adapter. Modes that are supported in the UI must listed.
+        /// meta data i.e. esb message custom propeties that will be supported must be added, each one preceeded by a period (.), followed by the
+        /// name of the property (no spaces), a semicolon, followed by the description of the property i.e.
+        ///     .[NameOfProperty].[Description of property]
+        /// These properties will be displayed in the Set Properties Process Step as well.
+        /// </remarks>
+        public MongoDbAdapter()
+        {
+            AdapterModes = new AdapterMode[]
+            { 
+                /// To control what is displayed and supporte in the neuron explorer UI, comment out the modes
+                /// that you do not wish to support
+                new AdapterMode(AdapterModeStringsEnum.Subscriber.Description(), MessageDirection.DatagramSender),      // subscribe mode - send messages to an MongoDb
+               
+                
+            };
+
+            ESBAdapterCapabilities caps = new ESBAdapterCapabilities();
+            caps.AdapterName = AdapterName;
+            caps.Prefix = MetadataPrefix;
+
+
+            // SAMPLE: Sample context properties that will be exposed within Neuron. These can be viewed within the Set Properties Process Step 
+            // **************************************************************
+            caps.MetadataFieldInfo =
+                MetadataPrefix + ".Database:Name of database is sent to or retreived from," +
+                MetadataPrefix + ".Collection:name of the collection for storing data,";
+           // **************************************************************
+            Capabilities = caps;
+
+        }
+
+        #region Base Class call outs for the Adapter Interfaces
+
+        /// <summary>
+        /// This is called by the base adapter class's Connect() method. This should contain all application specific validation
+        /// logic and initialization of resources used by the custom adapter.
+        /// if an exception is thrown, it will be caught by the runtime and the adapter will fail to start up. It will appear as 
+        /// error in the Neuron ESB Event log and will be in a stopped state within Endpoint Health.
+        /// </summary>
+        private void ConnectAdapter()
+        {
+            // SAMPLE:  Validation logic for basic properties should be done here
+            // ***********************************
+            
+            if (string.IsNullOrEmpty(Database)) throw new ArgumentNullException("Database", "You must enter the name of the database to connect to");
+            if (string.IsNullOrEmpty(Collection)) throw new ArgumentNullException("Collection", "You must enter the name of the collection to store data to.");
+
+
+            // SAMPLE: Custom set metadata properties that will be provided with every message sent or received from Neuron
+            // The IncludeMetadata flag is set by the "Include Metadata Properties" checkbox located on the General tab of an 
+            // Adapter Endpoint within the Neuron ESB Explorer.
+            // ***********************************
+            if (IncludeMetadata)
+            {
+                MessageProperties.Add(new NameValuePair("Database", Database));
+                MessageProperties.Add(new NameValuePair("Collection", Collection));
+            }
+
+            try
+            {
+                MongoClient client = new MongoClient(ConnectionString);
+                mongo = client.GetServer();
+                mongo.Connect();
+            }
+            catch (Exception ex)
+            {
+                RaiseAdapterInfo(ErrorLevel.Error, ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// This is called by the base adapter class's Disconnect() method
+        /// All resources that the custom adapter uses should be cleaned up here.
+        /// </summary>
+        private void DisconnectResources()
+        {
+            RaiseAdapterInfo(ErrorLevel.Info, "Disconnecting all resources");
+
+            try
+            {
+                // *** place clean up work here
+
+            }
+            catch (Exception ex)
+            {
+                RaiseAdapterError(ErrorLevel.Error,ex.Message, ex);
+            }
+        }
+
+
+        /// <summary>
+        /// Called from the base class's SendToEndpoint().  This be called by the Neuron ESB runtime for Subscription mode adapters
+        /// 
+        /// </summary>
+        /// <param name="message">ESB Message handed to adapter from runtime</param>
+        /// <param name="tx">Deprecated Property - This is no longer used and IS ALWAYS NULL</param>
+        private void SendToDataSource(ESBMessage message, CommittableTransaction tx)
+        {
+            RaiseAdapterInfo(ErrorLevel.Verbose, "Received Message. MessageId " + message.Header.MessageId + " Topic " + message.Header.Topic);
+
+            try
+            {
+                // if mode is solicit/Response, return a reply to the bus
+                switch (this._adapterMode.ModeName)
+                {
+                    case AdapterModeStringConstants.Subscribe:
+                        SendToDataSource(message);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException(string.Format("Error in {0}, mode {1} not supported", AdapterName, this._adapterMode.ModeName));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // throw the error to ensure the runtime enforces any adapter policies. Once an a policy executes
+                throw ex;
+            }
+        }
+
+
+
+        #region Send Functions
+
+        /// <summary>
+        /// called from SendToDataSource(). 
+        /// This is a one way send to a back end system, protocol, transport, etc
+        /// </summary>
+        private void SendToDataSource(ESBMessage message)
+        {
+            if (message.Text != String.Empty)
+            {
+                try
+                {
+                    if (mongo.State != MongoServerState.Disconnected)
+                    {
+                        if (mongo.DatabaseExists(Database))
+                        {
+                            var db = mongo.GetDatabase(Database);
+                            using (mongo.RequestStart(db))
+                            {
+                                if (db.CollectionExists(Collection))
+                                {
+                                    var collection = db.GetCollection(Collection);
+
+                                    var id = ObjectId.GenerateNewId();
+                                    BsonDocument msg = new BsonDocument()
+                                    .Add("_id", id)
+                                    .Add("message", message.Text)
+                                    .Add("Timestamp", DateTime.UtcNow.ToString("ddMMyyhhmmss"));
+                                    collection.Insert(msg);
+                                }
+                                else
+                                {
+                                    var result = db.CreateCollection(Collection);
+                                    if (result.Ok)
+                                    {
+                                        var collection = db.GetCollection(Collection);
+                                        var id = ObjectId.GenerateNewId();
+                                        BsonDocument msg = new BsonDocument()
+                                        .Add("_id", id)
+                                        .Add("message", message.Text);
+                                        collection.Insert(msg);
+                                    }
+                                    else
+                                    {
+                                        RaiseAdapterInfo(ErrorLevel.Error, "Collection failed to create. Reason :" + result.ErrorMessage);
+                                    }
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            RaiseAdapterInfo(ErrorLevel.Error, Database + ": Database doesn't exist");
+                        }
+                    }
+                    else
+                    {
+                        RaiseAdapterInfo(ErrorLevel.Error, "MongoDb Server is down");
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    // using multiple threads so raise error and audit 
+                    RaiseAdapterError(ErrorLevel.Error, string.Format("MongoDb adapter failed to log the message."), ex);
+
+                    throw ex;
+                }
+            }
+        }
+
+        #endregion
+
+
+
+        #endregion
+
+       
+    }
+}
---
+++ Quasar.Neuron.Adaptors.MongoDb/Neuron/Adapter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+using System.Transactions;
+using System.Globalization;
+using Neuron.Esb.Administration;
+
+namespace Neuron.Esb.Adapters
+{
+    /// <summary>
+    /// This is part of the Custom adapter class. Generally the developer would only edit the Class name.
+    /// everything else within this partial class are generic routines used to support the adapter
+    /// base class.
+    /// </summary>
+    [TypeConverter(typeof(PropertiesTypeConverter))]
+    public partial class MongoDbAdapter : ESBAdapterBase
+    {
+
+        #region Adapter Mode Enumeration and Classes
+        /// <summary>
+        /// Contains all the string constants for the modes the adapter supports so we
+        /// can always have known spellings. These will be displayed in the Mode dropdown control located
+        /// on the General Tab of an Adapter endpoint.  these are referenced in the
+        /// AdapterModeStringsEnum enumeration. Delete the ones that are not going to be supported
+        /// within the custom adapter
+        /// </summary>
+        public static class AdapterModeStringConstants
+        {
+            public const string Publish = "Publish";
+            public const string Subscribe = "Subscribe";
+            public const string SolicitResponse = "Solicit Response";
+            public const string RequestReply = "RequestReply";
+        }
+        /// <summary>
+        /// Contains all of the modes that the adapter supports. These are referenced in the 
+        /// Adapter constructor method
+        /// </summary>
+        public enum AdapterModeStringsEnum
+        {
+            [Description(AdapterModeStringConstants.Publish)]
+            Publish = 0,
+            [Description(AdapterModeStringConstants.Subscribe)]
+            Subscriber = 1,
+            [Description(AdapterModeStringConstants.SolicitResponse)]
+            SolicitResponse= 2,
+            [Description(AdapterModeStringConstants.RequestReply)]
+            RequestReply = 3
+        }
+        #endregion
+
+        #region Constants and types
+        /// <summary>
+        /// This is used for the receive thread that is started to support polling operations (i.e. Publish mode)
+        /// </summary>
+        private const int ThirtySeconds = 90 * 1000;                            // thread timeout for join
+        #endregion
+
+        #region private properties
+        private MessageAudit MessageAuditService { get; set; }
+        private ESBConfiguration Configuration { get; set; }
+        private List<NameValuePair> MessageProperties = new List<NameValuePair>();    // reference to the Neuron ESB custom message properties populated by custom adapter
+        private Thread _receiveThread;                                          // Receive thread used to start polling function to support default Pubish mode
+        private int _pollFailCount = 0;                                         // Used to track errors generated to support failure mode error handling options during polling
+        private bool _reportPollingErrors = true;                               // Determines whether or not to report the error during polling. used in conjunction with failure mode error handling options
+        private const int _maxPollFails = 1;                                    // defines maxmimum number of failures during polling before suppress consecutive errors option is executed
+        public AdapterMode _adapterMode;                                        // Set in the Connect() method. The adapter mode selected at design time on the general tab of the adapter endpoint
+        private ManualResetEvent _quitEvent = new ManualResetEvent(false);      // Used in polling mode to pause polling per PollingInterval property value
+        #endregion
+
+        /// <summary>
+        /// Default properties displayed in design time property grid of the adapter. The first property 'PublishTopic' 
+        /// controls what topic the message is publish to when the adapter is in Publish/Request-reply mode.  The 
+        /// remaining properties control before for Polling and how to handle errors when Polling.  if the custom 
+        /// adapter does not support polling and/or publish modes, these properties can be removed.
+        /// </summary>
+
+
+        #region Base class overrides
+        /// <summary>
+        /// This routine is fired by the ESB framework when the adapter is started up
+        /// </summary>
+        /// <param name="adapterMode">Contains the adapter mode being requested</param>
+        /// <remarks>
+        /// ESB framework will have already set the properties.  The requested
+        /// adapter mode is checked against supported modes for validity.
+        public override void Connect(string adapterMode)
+        {
+            this._adapterMode = null;
+
+            foreach (AdapterMode mode in AdapterModes)
+            {
+                // don't worry about capitalization
+                if (adapterMode.Equals(mode.ModeName,StringComparison.InvariantCultureIgnoreCase))
+                {
+                    this._adapterMode = mode;
+                    break;
+                }
+            }
+
+            // IncludeMetadata is a option located in the General Tab of the Adapter endpoint. Its meant to control whether or not
+            // custom message properties should be collected and persisted and/or retreived from the message
+            if (IncludeMetadata) MessageProperties.Add(new NameValuePair("Mode", this._adapterMode.ModeName));
+
+            // get a copy of the esb configuration.  Used primarily for custom error handling and for instantiating the Neuron Message 
+            // Audit service used for publish mode exceptions.
+            Configuration = this.Configuration;
+            RaiseAdapterInfo(ErrorLevel.Verbose, Configuration == null ? "ESB Configuration Is NULL" : "ESB Configuration returned");
+
+            // connect to Neuron audit service
+          
+
+            // call user defined method
+            ConnectAdapter();
+
+           
+
+           
+        }
+
+        public override void SendToEndpoint(ESBMessage message, CommittableTransaction tx)
+        {
+            // call user defined method subscribing to message received from the bus and sending out to some system
+            SendToDataSource(message, tx);
+        }
+        /// <summary>
+        /// This routine if fired by the ESB framework when the adapter is shutdown
+        /// </summary>
+        public override void Disconnect()
+        {
+            try
+            {
+                // call user defined method
+                DisconnectResources();
+
+              
+            }
+            catch(Exception ex)
+            {
+                string msg = string.Format("An error occurred releasing resources during the shutdown of the adapter,'{0}'. Exception: {1}", AdapterName,ex.ToString());
+                RaiseAdapterInfo(ErrorLevel.Warning, msg);
+            }
+
+            try
+            {
+                if (Configuration != null) Configuration = null;
+
+                if (MessageAuditService != null) MessageAuditService.Disconnect();
+                MessageAuditService = null;
+
+                this._quitEvent.Set();
+
+                // wait for thread to shutdown nicely, otherwise abort it so it isn't orphaned.  If you
+                // are using background threads this should not be an issue, but it can happen.
+                if (this._receiveThread != null && !this._receiveThread.Join(ThirtySeconds))
+                    this._receiveThread.Abort();
+            }
+            catch(ThreadAbortException) // we can ignore the thread abort
+            {}
+            catch (Exception ex)
+            {
+                string msg = string.Format("An error occurred while disconnecting from the data source during the shutdown of the adapter,'{0}'. Exception: {1}", AdapterName, ex.ToString());
+                RaiseAdapterInfo(ErrorLevel.Warning, msg);
+            }
+        }
+        #endregion
+
+   
+    }
+}
